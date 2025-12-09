244fc4e38d439807e3b9747a139fa8132f1f0020
kaustuvc <kaustuvchatterjee1234@gmail.com>
2025-12-09 22:11:49 +0530

    added component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import QueueForm from './components/QueueForm.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [queue, setQueue] = useState(0)
+  const enQueue = () => {
 
+  }
+  const updateStatus = () => {
+    
+  }
+  const deQueue = () => {
+
+  }
   return (
-    <>
-      
-    </>
+   <div>
+    <header>
+      <h1>Queue Management System</h1>
+      <p>Manage your customers efficiently</p>
+    </header>
+    <QueueForm />
+   </div>
   )
 }
 
---
+++ src/components/QueueForm.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+
+export default function QueueForm ({}) {
+    const [name, setName] = useState([])
+    const [service, setService] = useState('')
+    return (
+        <h1>Form Component</h1>
+    )
+}
